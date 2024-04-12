353d71c748f428e96942848b29cb375a8f57393a
Ankush Kumar <94294476+ankushkmrsingh@users.noreply.github.com>
2024-04-12 15:00:09 +0530

    Add files via upload

---
+++ 19.GettingThingsOutOfFuntions/19.9ReturnTypeDeductionWithReferences/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+
+auto& max(int& a, int& b){
+    if(a>b){
+        return a;
+    }else{
+        return b; // Will return a copy.
+    }
+}
+
+int main(){
+
+
+    int x{10};
+    int y{45};
+
+	int& result = max(x,y); // Error : Can not treat return value as a reference if int is returned instead of int&
+							
+	//int result = max(x,y); // A copy of the max is returned
+	
+	++ result;
+	
+	std::cout << "x :" << x << std::endl; // 10
+	std::cout << "y :" << y << std::endl; // 46
+	std::cout << "result : " << result << std::endl; // 46
+   
+    return 0;
+}
